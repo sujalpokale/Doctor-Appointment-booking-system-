414d173365b127a94b3979fc0a37c91c5721a4ed
sujalpokale <156334934+sujalpokale@users.noreply.github.com>
2026-06-03 21:16:07 +0530

    Fixed MongoDB connection string

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    mongoose.connection.on('connected', () =>
+        console.log("Database Connected")
+    );
 
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
-
+    await mongoose.connect(process.env.MONGODB_URI);
 }
 
 export default connectDB;
-
-// Do not use '@' symbol in your databse user's password else it will show an error.
